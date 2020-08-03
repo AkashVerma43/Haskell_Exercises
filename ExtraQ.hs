a7db7fdd5f131c872676cd6dd7ba65b954b79091
Akash Verma <52218909+AkashVerma43@users.noreply.github.com>
2020-08-04 02:57:48 +0530

    Update ExtraQ.hs

--- ExtraQ.hs
+++ ExtraQ.hs
@@ -30,14 +30,67 @@ data R r a = R (r -> a)
 instance Functor (R r) where
   fmap f (R g) = R (f . g)
 
---Couldn't able to deduce what would be pure in applicative, same with below question
+instance Applicative (R r) where
+  pure a = R (\_ -> a)
+  (R f) <*> (R g) = R (\x -> f x (g x))
+
+instance Monad (R r) where
+  return = pure
+  (R g) >>= f = R $ (\r -> (let R h = f (g r) in (h r)))
+
+--f :: a -> R r b
+--g :: r -> a
+
 --3
 data C f g a = C (f (g a)) deriving (Show)
 
 instance (Functor f, Functor g) => Functor (C f g) where
   fmap h (C fga) = C ((fmap . fmap) h fga)
 
+instance (Applicative f, Applicative g) => Applicative (C f g) where
+  pure x = C (pure (pure x))
+  (C fgh) <*> (C fga) = C (((fmap) (<*>) fgh) <*> fga)
+
+instance (Monad f, Monad g) => Monad (C f g) where
+  return = pure
+  C fga >>= f = do
+    a <- C fga
+    (f a)
+
+--4
+--data P a = P (a->Bool)
+
 --5
---data S = S(s -> (a,s))
---not in scope : type variable 'a'
---not in scope : type variable 's'
+data S s a = S (s -> (a, s))
+
+instance Functor (S s) where
+  fmap f (S g) = S $ (\s -> (let (a, x) = g s in (f a, x)))
+
+instance Monoid s => Applicative (S s) where
+  pure a = S (\_ -> (a, mempty))
+  (S f) <*> (S g) =
+    S $
+      ( \s ->
+          ( let (a, x) = g s
+                (h, x') = f s
+             in (fst (h, x') a, snd (h, x') <> x)
+          )
+      )
+
+--g :: s -> (a , s)
+--f :: s -> (a->b,s)
+-- <*> :: S s (a->b) -> S s a -> S s b
+
+instance Monoid s => Monad (S s) where
+  return = pure
+  (S g) >>= f =
+    S $
+      ( \s ->
+          ( let (a, x) = g s
+                S h = f a
+                (b, x') = h s
+             in (b, x <> x')
+          )
+      )
+
+--f :: a -> S (s->(b,s))
