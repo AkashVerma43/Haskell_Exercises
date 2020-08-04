04407682899ec447c347268ad311fba049d1f80b
Akash Verma <52218909+AkashVerma43@users.noreply.github.com>
2020-08-04 17:12:24 +0530

    Add files via upload

---
+++ Chap20Exercise.hs
@@ -0,0 +1,73 @@
+module Chap20Exercise where
+
+--import Data.Foldable
+import Data.Monoid
+
+--Library Functions
+
+--
+sum' :: (Foldable t, Num a) => t a -> a
+sum' = getSum . foldMap Sum
+
+--or
+--sum' = foldr (+) 0
+
+--
+product' :: (Foldable t, Num a) => t a -> a
+product' = getProduct . foldMap Product
+
+--or
+--product' = foldr (*) 1
+
+--
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' x = foldr ((||) . (x ==)) False
+
+--or
+--elem' x  = getAny.foldMap (Any.(x==))
+
+--
+length' :: (Foldable t) => t a -> Int
+length' = foldr (\_ l -> l + 1) 0
+
+--
+toList' :: (Foldable t) => t a -> [a]
+toList' = foldr ((++) . (\x -> [x])) []
+
+--
+fold' :: (Foldable t, Monoid m) => t m -> m
+fold' = foldMap id
+
+--Foldable instances of following types
+--Just an Example from the chapter
+data Optional a = Nada | Yep a deriving (Show)
+
+instance Foldable Optional where
+  foldMap _ Nada = mempty
+  foldMap f (Yep a) = f a
+
+--1
+data Constant a b = Constant a deriving (Show)
+
+instance Foldable (Constant a) where
+  foldMap _ (Constant _) = mempty
+
+--2
+data Two a b = Two a b deriving (Show)
+
+instance Foldable (Two a) where
+  foldMap f (Two _ b) = f b
+
+--  fold (Two _ b) = b
+
+--3
+data Three a b c = Three a b c deriving (Show)
+
+instance Foldable (Three a b) where
+  foldMap f (Three _ _ c) = f c
+
+--4
+data Three' a b = Three' a b b deriving (Show)
+
+instance Foldable (Three' a) where
+  foldMap f (Three' _ b b') = (f b) <> (f b')
